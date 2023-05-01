8efdfa169d46a1edb34fbcb1a35b561297a8405f
Marc <marcojuhel@gmail.com>
2023-05-01 19:19:33 +0200

    first commit

---
+++ app/src/main/java/fr/marc/totoapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.marc.totoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
